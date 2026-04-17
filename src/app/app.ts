9110b9a74cf22344d44f4dbd131b01f79e1681d4
Kam <erkamyaman35@gmail.com>
2026-04-17 22:46:28 +0300

    Remove unused PrimeNG button module and related code from app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('starter');
-
-  protected onPrimeClick(): void {
-    console.log('PrimeNG button clicked');
-  }
-}
+export class App {}
